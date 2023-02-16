c4ef6bda63b96a3eac86208b85e72f250ac572ff
Bigst3pp3r <mltnkarani@gmail.com>
2023-02-16 20:43:06 +0000

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
   * main - prints the size of various types on the computer
   * it is compiled and run on.
-  * Return: Always 0 (Success)
+  * Return: Always 0
   */
 
 int main(void)
 {
-printf("The size of char is %zu byte(s)\n", sizeof(char));
-printf("The size of int is %zu byte(s)\n", sizeof(int));
-printf("The size of long is %zu byte(s)\n", sizeof(long));
-printf("The size of long long is %zu byte(s)\n", sizeof(long long));
-printf("The size of float is %zu byte(s)\n", sizeof(float));
+printf("Size of char is: %zu byte(s)\n", sizeof(char));
+printf("Size of int is: %zu byte(s)\n", sizeof(int));
+printf("Size of long is: %zu byte(s)\n", sizeof(long));
+printf("Size of long long is: %zu byte(s)\n", sizeof(long long));
+printf("Size of float is: %zu byte(s)\n", sizeof(float));
 return (0);
 }
