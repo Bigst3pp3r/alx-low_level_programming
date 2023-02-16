7db8b322ae154692f935c76e6ba9a2372dc01838
Bigst3pp3r <mltnkarani@gmail.com>
2023-02-16 20:37:33 +0000

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,9 @@
 int main(void)
 {
 printf("The size of char is %zu byte(s)\n", sizeof(char));
-printf("The size of short is %zu byte(s)\n", sizeof(short));
 printf("The size of int is %zu byte(s)\n", sizeof(int));
 printf("The size of long is %zu byte(s)\n", sizeof(long));
 printf("The size of long long is %zu byte(s)\n", sizeof(long long));
 printf("The size of float is %zu byte(s)\n", sizeof(float));
-printf("The size of double is %zu byte(s)\n", sizeof(double));
-printf("The size of long double is %zu byte(s)\n", sizeof(long double));
 return (0);
 }
