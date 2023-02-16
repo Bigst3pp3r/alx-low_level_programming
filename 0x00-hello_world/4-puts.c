ae95fc0bedd96f79f4e6bba06aba41e5dc058c89
Bigst3pp3r <mltnkarani@gmail.com>
2023-02-16 19:42:27 +0000

    puts-function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line."
- * return 0.
+ * puzzle, followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle\n");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0); }
