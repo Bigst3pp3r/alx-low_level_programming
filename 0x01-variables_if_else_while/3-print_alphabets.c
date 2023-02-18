a730d7d42a87d3941cd41d0be2e6b7f685c648c4
Bigst3pp3r <mltnkarani@gmail.com>
2023-02-18 03:10:56 +0000

    prints-alphabe-in-caps-the-no-caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+   * main - This program prints alphabet in lowercase then uppercase
+   *
+   * Return: Always 0
+   */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+
