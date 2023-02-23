b0701073adf553fccd1ef0cc655aee60cbfb01df
Bigst3pp3r <mltnkarani@gmail.com>
2023-02-23 14:46:52 +0000

    0tasks

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,19 +7,12 @@
 
 int main(void)
 {
-<<<<<<< HEAD
+
 int i;
 
 i = 98;
 positive_or_negative(i);
 
 return (0);
-} 
-=======
-        int i;
+}
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);}
->>>>>>> bb1f4b76f99d1d944cef8e252dd1efdb1d28e520
